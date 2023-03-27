50781070be714669ed6db12d8838a4d8a641d091
Alexander Baumstark <alexander.baumstark@tu-ilmenau.de>
2023-03-27 13:50:12 +0000

    Added basic interface for dbscan

--- dbscan/common/db.h
+++ dbscan/common/db.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 
 #define CHUNK_SIZE 65536
+#define RECORDS 817
 
 struct student_record {
     uint64_t id;    
@@ -10,12 +11,12 @@ struct student_record {
 } S_Record;
 
 struct student_chunk {
-    struct student_record records[817];
+    struct student_record records[RECORDS];
     struct student_chunk *next;
     uint64_t count;
 } S_Chunk;
 
-void add_record(student_chunk *sc, student_record *sr) {
+void chunk_add_record(student_chunk *sc, student_record *sr) {
     sc->records[sc->count++] = *sr;
 }
 
@@ -27,3 +28,27 @@ struct database {
     char name[32];
     struct S_Chunk *data;
 };
+
+struct database* new_database() {
+    auto db = new database();
+    db->data = new student_chunk();
+    return db;
+}
+
+void db_add_record(struct database* db, struct student_record* sr) {
+    struct student_chunk* prev = nullptr;
+    auto cur = db->data;
+    
+    while(cur != nullptr) {
+        if(cur->count == RECORDS) {
+            prev = cur;
+            cur = cur->next;
+        } else {
+            chunk_add_record(cur, sr);
+            return;
+        }
+    }
+
+    prev->next = alloc_chunk();
+    chunk_add_record(prev->next, sr);
+}
--- dbscan/host/main.cpp
+++ dbscan/host/main.cpp
@@ -2,11 +2,15 @@
 #include <dpu_management.h>
 
 #include <stdexcept>
-#include <sstream> 
+#include <sstream>
+#include <chrono> 
 
 #include "../common/db.h"
 
-void load_csv(const std::string &path, struct database *db) {
+typedef std::vector<struct student_records> result_set;
+
+std::size_t load_csv(const std::string &path, struct database *db) {
+    std::size_t count = 0;
     std::ifstream file(path);
 
     if(!file.is_open()) {
@@ -40,21 +44,64 @@ void load_csv(const std::string &path, struct database *db) {
             }
             colType++;
         }
+        db_add_record(db, sr);
+        count++;
     }
+    return count;
 }
 
+void transfer_to_dpu(struct database *db, dpu_set_t &set) {
 
+}
+
+void run_dpu(dpu_set_t &set) {
+
+}
+
+void transfer_from_dpu(dpu_set_t &set, result_set &rs) {
+
+}
+
+void run_cpu(result_set &rs) {
+
+}
 
 int main() {
+    auto db = new_database();
+
+    dpu_set_t set, dpu;
+
     // TODO:
     // 1. Load csv into DB.
+    load_csv("student_data.csv", db);
 
     // 2. Initialize DPU environment.
 
     // 3. Push data to MRAM of DPUs.
+    auto start_transfer = std::chrono::high_resolution_clock::now();
+    transfer_to_dpu(db, set);
+    auto end_transfer = std::chrono::high_resolution_clock::now();
 
     // 4. Run DPUs.
+    auto start_launch = std::chrono::high_resolution_clock::now();
+    run_dpu(set);
+    auto end_launch = std::chrono::high_resolution_clock::now();
 
     // 5. Obtain results from DPUs.
+    result_set rs;
+    auto start_transfer2 = std::chrono::high_resolution_clock::now();
+    transfer_from_dpu(set, rs);
+    auto end_transfer2 = std::chrono::high_resolution_clock::now();
+
+    result_set rs2;
+    auto start_cpu = std::chrono::high_resolution_clock::now();
+    run_cpu(rs);
+    auto end_cpu = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Transfer to:      " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer-start_transfer).count() << " ms\n"
+              << "DPU time:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_launch-start_launch).count() << " ms\n"
+              << "Transfer from:    " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer2-start_transfer2).count() << " ms\n" 
+              << "Baseline:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_cpu-start_cpu).count() << " ms\n"      
+
     return 0;
 }
