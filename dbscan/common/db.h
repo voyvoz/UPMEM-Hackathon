1181934215bf65352baab05dc53844ad0db05cd4
Alexander Baumstark <alexander.baumstark@tu-ilmenau.de>
2023-03-29 08:07:29 +0000

    Makefile added to dbscan

--- dbscan/common/db.h
+++ dbscan/common/db.h
@@ -16,39 +16,7 @@ struct student_chunk {
     uint64_t count;
 } S_Chunk;
 
-void chunk_add_record(student_chunk *sc, student_record *sr) {
-    sc->records[sc->count++] = *sr;
-}
-
-struct student_chunk *alloc_chunk() {
-    return new student_chunk;
-}
-
 struct database {
     char name[32];
-    struct S_Chunk *data;
+    struct student_chunk *data;
 };
-
-struct database* new_database() {
-    auto db = new database();
-    db->data = new student_chunk();
-    return db;
-}
-
-void db_add_record(struct database* db, struct student_record* sr) {
-    struct student_chunk* prev = nullptr;
-    auto cur = db->data;
-    
-    while(cur != nullptr) {
-        if(cur->count == RECORDS) {
-            prev = cur;
-            cur = cur->next;
-        } else {
-            chunk_add_record(cur, sr);
-            return;
-        }
-    }
-
-    prev->next = alloc_chunk();
-    chunk_add_record(prev->next, sr);
-}
--- dbscan/host/main.cpp
+++ dbscan/host/main.cpp
@@ -4,10 +4,18 @@
 #include <stdexcept>
 #include <sstream>
 #include <chrono> 
+#include <string.h>
+#include <fstream>
+#include <iostream>
 
 #include "../common/db.h"
 
-typedef std::vector<struct student_records> result_set;
+typedef std::vector<struct student_records*> result_set;
+
+void chunk_add_record(student_chunk *sc, student_record *sr);
+struct student_chunk *alloc_chunk();
+void db_add_record(struct database* db, struct student_record* sr);
+struct database* new_database();
 
 std::size_t load_csv(const std::string &path, struct database *db) {
     std::size_t count = 0;
@@ -24,24 +32,15 @@ std::size_t load_csv(const std::string &path, struct database *db) {
 
         int colType = 0;
         
-        student_record sr;
+        student_record *sr = new student_record();
 
-        while(ss >> data) {
-            switch(colType) {
-                case 0: {
-                    sr.id = std::stoi(data);
-                    break;
-                }
-                case 1: {
-                    sr.firstname = data;
-                }
-                case 2: {
-                    sr.lastname = data;
-                }
-                case 3: {
-                    sr.age = std::stoi(data);
-                }
-            }
+        while(ss.good()) {
+            std::getline(ss, data, ',');
+            if(colType == 0) sr->id = std::stoi(data);
+            else if(colType == 1) strcpy(sr->firstname, data.c_str());
+            else if(colType == 2) strcpy(sr->lastname, data.c_str());
+            else if(colType == 3) sr->age = std::stoi(data);
+            if(ss.peek() == ',') ss.ignore();
             colType++;
         }
         db_add_record(db, sr);
@@ -73,7 +72,8 @@ int main() {
 
     // TODO:
     // 1. Load csv into DB.
-    load_csv("student_data.csv", db);
+    auto count = load_csv("./student_data.csv", db);
+    std::cout << "Loaded " << count << " items into DB" << std::endl;
 
     // 2. Initialize DPU environment.
 
@@ -98,10 +98,42 @@ int main() {
     run_cpu(rs);
     auto end_cpu = std::chrono::high_resolution_clock::now();
 
-    std::cout << "Transfer to:      " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer-start_transfer).count() << " ms\n"
-              << "DPU time:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_launch-start_launch).count() << " ms\n"
-              << "Transfer from:    " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer2-start_transfer2).count() << " ms\n" 
-              << "Baseline:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_cpu-start_cpu).count() << " ms\n"      
+    std::cout << "Transfer to:      " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer-start_transfer).count() << " ms" << std::endl;
+    std::cout << "DPU time:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_launch-start_launch).count() << " ms" << std::endl;
+    std::cout << "Transfer from:    " << std::chrono::duration_cast<std::chrono::milliseconds>(end_transfer2-start_transfer2).count() << " ms" << std::endl;
+    std::cout << "Baseline:         " << std::chrono::duration_cast<std::chrono::milliseconds>(end_cpu-start_cpu).count() << " ms" << std::endl;      
 
     return 0;
+}
+
+void chunk_add_record(student_chunk *sc, student_record *sr) {
+    sc->records[sc->count++] = *sr;
+}
+
+struct student_chunk *alloc_chunk() {
+    return new student_chunk;
+}
+
+struct database* new_database() {
+    auto db = new database();
+    db->data = new student_chunk();
+    return db;
+}
+
+void db_add_record(struct database* db, struct student_record* sr) {
+    struct student_chunk* prev = nullptr;
+    auto cur = db->data;
+    
+    while(cur != nullptr) {
+        if(cur->count == RECORDS) {
+            prev = cur;
+            cur = cur->next;
+        } else {
+            chunk_add_record(cur, sr);
+            return;
+        }
+    }
+
+    prev->next = alloc_chunk();
+    chunk_add_record(prev->next, sr);
 }
